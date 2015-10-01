1e3a8f3dbdbe321e3d307166829d1d0c02596a8a
Seb Maynard <github@seb.so>
2015-10-01 22:30:25 +0100

    Made the 'available_roles' use the user.options value
    In UserController.php

--- src/SimpleUser/UserController.php
+++ src/SimpleUser/UserController.php
@@ -469,7 +469,7 @@ class UserController
             'layout_template' => $this->getTemplate('layout'),
             'error' => implode("\n", $errors),
             'user' => $user,
-            'available_roles' => array('ROLE_USER', 'ROLE_ADMIN'),
+            'available_roles' => $app['user.options']['roles'],
             'image_url' => $this->getGravatarUrl($user->getEmail()),
             'customFields' => $customFields,
             'isUsernameRequired' => $this->isUsernameRequired,
